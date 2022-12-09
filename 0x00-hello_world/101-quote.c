0f8a3904091addba7abf81aee115ccdfc899bbb8
Your Nameyohbi2022git config user.name git clone https://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@gitclearroot@c174ab32603f:/alx-pre_course# git config --global user.name Your <ybirara@yahoo.com>
2022-12-09 02:27:37 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-        fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout task8);
+        fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
 	return (1);
 }

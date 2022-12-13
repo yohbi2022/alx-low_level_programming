110201cd2894d24577563ff629617d3961b00917
Your Nameyohbi2022git config user.name git clone https://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@gitclearroot@c174ab32603f:/alx-pre_course# git config --global user.name Your <ybirara@yahoo.com>
2022-12-13 09:01:03 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,7 @@
  */
 
 int main(void)
-
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -19,7 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
-
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int _abs(int);
 int print_last_digit(int);
 void times_table(void);
 int add(int, int);
-int _putchar(char c)
+int _putchar(char c);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
